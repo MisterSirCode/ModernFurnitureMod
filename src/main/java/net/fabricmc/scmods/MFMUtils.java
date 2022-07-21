b217a06a0534c9a781a8a0e79db99f405f3c91fc
Sir Code <taylorschneider07@yahoo.com>
2022-07-21 03:42:56 -0400

    Add other wood types

--- src/main/java/net/fabricmc/scmods/MFMUtils.java
+++ src/main/java/net/fabricmc/scmods/MFMUtils.java
@@ -20,6 +20,12 @@ public class MFMUtils implements ModInitializer {
 
 	//public static final Item fuel = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     public static final Block oak_table = new Block(FabricBlockSettings.of(Material.WOOD, Blocks.OAK_PLANKS.getDefaultMapColor()).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque());
+    public static final Block birch_table = new Block(FabricBlockSettings.of(Material.WOOD, Blocks.BIRCH_PLANKS.getDefaultMapColor()).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque());
+    public static final Block spruce_table = new Block(FabricBlockSettings.of(Material.WOOD, Blocks.SPRUCE_PLANKS.getDefaultMapColor()).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque());
+    public static final Block jungle_table = new Block(FabricBlockSettings.of(Material.WOOD, Blocks.JUNGLE_PLANKS.getDefaultMapColor()).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque());
+    public static final Block dark_oak_table = new Block(FabricBlockSettings.of(Material.WOOD, Blocks.DARK_OAK_PLANKS.getDefaultMapColor()).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque());
+    public static final Block acacia_table = new Block(FabricBlockSettings.of(Material.WOOD, Blocks.ACACIA_PLANKS.getDefaultMapColor()).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque());
+    public static final Block mangrove_table = new Block(FabricBlockSettings.of(Material.WOOD, Blocks.MANGROVE_PLANKS.getDefaultMapColor()).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque());
 
     @Override
     public void onInitialize() {
@@ -28,5 +34,23 @@ public class MFMUtils implements ModInitializer {
         Registry.register(Registry.BLOCK, new Identifier("mfm_utils", "oak_table"), oak_table);
         Registry.register(Registry.ITEM, new Identifier("mfm_utils", "oak_table"), 
             new BlockItem(oak_table, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("mfm_utils", "birch_table"), birch_table);
+        Registry.register(Registry.ITEM, new Identifier("mfm_utils", "birch_table"), 
+            new BlockItem(birch_table, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("mfm_utils", "spruce_table"), spruce_table);
+        Registry.register(Registry.ITEM, new Identifier("mfm_utils", "spruce_table"), 
+            new BlockItem(spruce_table, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("mfm_utils", "jungle_table"), jungle_table);
+        Registry.register(Registry.ITEM, new Identifier("mfm_utils", "jungle_table"), 
+            new BlockItem(jungle_table, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("mfm_utils", "dark_oak_table"), dark_oak_table);
+        Registry.register(Registry.ITEM, new Identifier("mfm_utils", "dark_oak_table"), 
+            new BlockItem(dark_oak_table, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("mfm_utils", "acacia_table"), acacia_table);
+        Registry.register(Registry.ITEM, new Identifier("mfm_utils", "acacia_table"), 
+            new BlockItem(acacia_table, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("mfm_utils", "mangrove_table"), mangrove_table);
+        Registry.register(Registry.ITEM, new Identifier("mfm_utils", "mangrove_table"), 
+            new BlockItem(mangrove_table, new FabricItemSettings().group(ItemGroup.MISC)));
     }
 }
