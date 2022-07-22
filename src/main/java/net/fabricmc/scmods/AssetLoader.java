21de2110f4d60ce810cb8dd793f588ecc68119d9
Sir Code <taylorschneider07@yahoo.com>
2022-07-22 11:26:31 -0400

    update some virtualization crap

---
+++ src/main/java/net/fabricmc/scmods/AssetLoader.java
@@ -0,0 +1,43 @@
+package net.fabricmc.scmods;
+
+import java.util.ArrayList;
+import net.devtech.arrp.api.RRPPreGenEntrypoint;
+import net.devtech.arrp.api.RuntimeResourcePack;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+
+
+public class AssetLoader implements RRPPreGenEntrypoint {
+    public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create("scmmfmutils:main");
+    public static ArrayList<String> woodItems = new ArrayList<String>();
+    public static ArrayList<String> woodItemIds = new ArrayList<String>();
+    public static final String vanillaLogs[] = {
+        "oak", "birch", "spruce", "jungle", "dark_oak", "acacia", "mangrove", "warped", "crimson", 
+        "stripped_oak", "stripped_birch", "stripped_spruce", "stripped_jungle", "stripped_dark_oak", "stripped_acacia", 
+        "stripped_mangrove", "stripped_warped", "stripped_crimson"
+    };
+
+    // Model Generator for all my wood furnitures
+    public static String createWoodModelJson(String type, String id, Boolean stem) {
+        if (stem == true) id += "_stem";
+        else id += "_log";
+        return "{\n" +
+                "    \"parent\": \"mfm_utils:block/" + type + ",\"\n" +
+                "    \"textures\": {\n" +
+                "        \"top\": \"minecraft:block/" + id + "_top\"," +
+                "        \"sides\": \"minecraft:block/" + id + "\"," +
+                "}";
+    }
+    
+    @Override
+    public void pregen() {
+        // Add everything to ARRP to load assets during runtimeS
+        for (int i = 0; i < vanillaLogs.length - 1; i++) {
+            String bid = vanillaLogs[i];
+            Boolean stem = false;
+            if (bid.contains("warped") || bid.contains("crimson")) stem = true;
+            byte[] data = createWoodModelJson("table", bid, stem).getBytes();
+            RESOURCE_PACK.addResource(ResourceType.CLIENT_RESOURCES, new Identifier("mfm_utils", "/block/" + bid + "_table"), data);
+        }
+    }
+}
--- src/main/java/net/fabricmc/scmods/MFMUtils.java
+++ src/main/java/net/fabricmc/scmods/MFMUtils.java
@@ -12,17 +12,10 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import java.util.ArrayList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MFMUtils implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("scmmfmutils");
-
-    public static final String woodLogs[] = {
-        "oak", "birch", "spruce", "jungle", "dark_oak", "acacia", "mangrove", "warped", "crimson"
-    };
-    
-    public static final String strippedLogs[] = {
+    public static final String vanillaLogs[] = {
+        "oak", "birch", "spruce", "jungle", "dark_oak", "acacia", "mangrove", "warped", "crimson", 
         "stripped_oak", "stripped_birch", "stripped_spruce", "stripped_jungle", "stripped_dark_oak", "stripped_acacia", 
         "stripped_mangrove", "stripped_warped", "stripped_crimson"
     };
@@ -32,47 +25,21 @@ public class MFMUtils implements ModInitializer {
         Blocks.ACACIA_PLANKS, Blocks.MANGROVE_PLANKS, Blocks.WARPED_PLANKS, Blocks.CRIMSON_PLANKS
     };
 
-    // Create Log Tables
+    // Create Tables
     public static ArrayList<Block> woodTables = new ArrayList<Block>();
-    static { for (int i = 0; i < woodLogs.length; i++) {
-        woodTables.add(new Table(FabricBlockSettings.of(Material.WOOD, woodBlocks[i].getDefaultMapColor())
-        .strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque()));
-    } };
-
-    // Create Stripped Tables
-    public static ArrayList<Block> strippedTables = new ArrayList<Block>();
-    static { for (int i = 0; i < woodLogs.length; i++) {
-        strippedTables.add(new Table(FabricBlockSettings.of(Material.WOOD, woodBlocks[i].getDefaultMapColor())
+    static { for (int i = 0; i < vanillaLogs.length; i++) {
+        woodTables.add(new Table(FabricBlockSettings.of(Material.WOOD, woodBlocks[i % 9].getDefaultMapColor())
         .strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque()));
     } };
 
     @Override
     public void onInitialize() {
         // Add Log Tables
-        for (int i = 0; i < woodLogs.length; i++) {
-            String currentTableName = woodLogs[i] + "_table";
+        for (int i = 0; i < vanillaLogs.length; i++) {
+            String currentTableName = vanillaLogs[i] + "_table";
             Registry.register(Registry.BLOCK, new Identifier("mfm_utils", currentTableName), woodTables.get(i));
             Registry.register(Registry.ITEM, new Identifier("mfm_utils", currentTableName), 
                 new BlockItem(woodTables.get(i), new FabricItemSettings().group(ItemGroup.DECORATIONS)));
         }
-
-        // Add Stripped Tables
-        for (int i = 0; i < woodLogs.length; i++) {
-            String currentTableName = strippedLogs[i] + "_table";
-            Registry.register(Registry.BLOCK, new Identifier("mfm_utils", currentTableName), strippedTables.get(i));
-            Registry.register(Registry.ITEM, new Identifier("mfm_utils", currentTableName), 
-                new BlockItem(strippedTables.get(i), new FabricItemSettings().group(ItemGroup.DECORATIONS)));
-        }
-    }
-
-    public static String createWoodModelJson(String type, String id, Boolean stem) {
-        if (stem == true) id += "_stem";
-        else id += "_log";
-        return "{\n" +
-                "    \"parent\": \"mfm_utils:block/" + type + ",\"\n" +
-                "    \"textures\": {\n" +
-                "        \"top\": \"minecraft:block/" + id + "_top\"," +
-                "        \"sides\": \"minecraft:block/" + id + "\"," +
-                "}";
     }
 }
