655b6015c27731b847a2a458c08fac11485cecb9
Sir Code <taylorschneider07@yahoo.com>
2022-07-22 21:15:25 -0400

    Update AssetEntrypoint.java

---
+++ src/main/java/net/scmods/AssetEntrypoint.java
@@ -0,0 +1,32 @@
+package net.scmods;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.block.Block;
+import net.minecraft.client.render.model.json.ItemModelGenerator;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.scmods.MFMUtils;
+
+
+public class AssetEntrypoint implements DataGeneratorEntrypoint {
+
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator dataGenerator) {
+        
+		dataGenerator.addProvider(WoodModelProvider::new);
+    }
+
+	private static class WoodModelProvider extends FabricModelProvider {
+		private WoodModelProvider(FabricDataGenerator generator) {
+			super(generator);
+		}
+
+		@Override
+		public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+            
+            for (int i = 0; i < MFMUtils.vanillaLogs.length; i++) {
+                blockStateModelGenerator.registerSimpleCubeAll(MFMUtils.woodTables.get(i));
+            }
+		}
+	}
+}
