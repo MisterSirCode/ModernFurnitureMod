84c4090ef0cf74fcd9a89c07bfe70561eec912cd
Sir Code <taylorschneider07@yahoo.com>
2022-07-23 23:10:19 -0400

    Update assets, add benches

---
+++ src/main/java/net/scmods/Bench.java
@@ -0,0 +1,27 @@
+package net.scmods;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class Bench extends Block {
+    public static final VoxelShape benchShape = VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(
+		VoxelShapes.cuboid(0.125, 0.4375, 0.125, 0.875, 0.5625, 0.875), 
+		VoxelShapes.cuboid(0.6875, 0, 0.1875, 0.8125, 0.4375, 0.3125)), 
+		VoxelShapes.cuboid(0.6875, 0, 0.6875, 0.8125, 0.4375, 0.8125)), 
+		VoxelShapes.cuboid(0.1875, 0, 0.6875, 0.3125, 0.4375, 0.8125)), 
+		VoxelShapes.cuboid(0.1875, 0, 0.1875, 0.3125, 0.4375, 0.3125));
+
+    public Bench(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return benchShape;
+    }
+}
--- src/main/java/net/scmods/MFMUtils.java
+++ src/main/java/net/scmods/MFMUtils.java
@@ -24,21 +24,28 @@ public class MFMUtils implements ModInitializer {
         Blocks.ACACIA_PLANKS, Blocks.MANGROVE_PLANKS, Blocks.WARPED_PLANKS, Blocks.CRIMSON_PLANKS
     };
 
-    // Create Tables
+    // Create Furniture
     public static ArrayList<Block> woodTables = new ArrayList<Block>();
+    public static ArrayList<Block> woodBenches = new ArrayList<Block>();
     static { for (int i = 0; i < vanillaLogs.length; i++) {
         woodTables.add(new Table(FabricBlockSettings.of(Material.WOOD, woodBlocks[i % 9].getDefaultMapColor())
+        .strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque()));
+        woodBenches.add(new Bench(FabricBlockSettings.of(Material.WOOD, woodBlocks[i % 9].getDefaultMapColor())
         .strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).nonOpaque()));
     } };
 
     @Override
     public void onInitialize() {
-        // Add Log Tables
+        // Add Furniture
         for (int i = 0; i < vanillaLogs.length; i++) {
             String currentTableName = vanillaLogs[i] + "_table";
             Registry.register(Registry.BLOCK, new Identifier("mfm_utils", currentTableName), woodTables.get(i));
             Registry.register(Registry.ITEM, new Identifier("mfm_utils", currentTableName), 
                 new BlockItem(woodTables.get(i), new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+            String currentBenchName = vanillaLogs[i] + "_table";
+            Registry.register(Registry.BLOCK, new Identifier("mfm_utils", currentBenchName), woodBenches.get(i));
+            Registry.register(Registry.ITEM, new Identifier("mfm_utils", currentBenchName), 
+                new BlockItem(woodBenches.get(i), new FabricItemSettings().group(ItemGroup.DECORATIONS)));
         }
     }
 }
