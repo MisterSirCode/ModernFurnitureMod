e091fc276243f2e48dea5043b6b3379d07081f8a
Sir Code <taylorschneider07@yahoo.com>
2022-07-25 06:46:13 -0400

    Update some waterlogging stuff

--- src/main/java/net/scmods/Table.java
+++ src/main/java/net/scmods/Table.java
@@ -3,29 +3,58 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.Waterloggable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class Table extends Block implements Waterloggable {
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
-    protected final VoxelShape boundingShape;
+    protected final VoxelShape boundingShape = VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(
+        VoxelShapes.cuboid(0, 0.875, 0, 1, 1, 1), 
+        VoxelShapes.cuboid(0.8125, 0, 0.0625, 0.9375, 0.9375, 0.1875)), 
+        VoxelShapes.cuboid(0.8125, 0, 0.8125, 0.9375, 0.9375, 0.9375)), 
+        VoxelShapes.cuboid(0.0625, 0, 0.8125, 0.1875, 0.9375, 0.9375)), 
+        VoxelShapes.cuboid(0.0625, 0, 0.0625, 0.1875, 0.9375, 0.1875));
 
     public Table(Settings settings) {
         super(settings);
-        boundingShape = VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(
-            VoxelShapes.cuboid(0, 0.875, 0, 1, 1, 1), 
-            VoxelShapes.cuboid(0.8125, 0, 0.0625, 0.9375, 0.9375, 0.1875)), 
-            VoxelShapes.cuboid(0.8125, 0, 0.8125, 0.9375, 0.9375, 0.9375)), 
-            VoxelShapes.cuboid(0.0625, 0, 0.8125, 0.1875, 0.9375, 0.9375)), 
-            VoxelShapes.cuboid(0.0625, 0, 0.0625, 0.1875, 0.9375, 0.1875));
+        this.setDefaultState(((BlockState)this.stateManager.getDefaultState()).with(WATERLOGGED, false));
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (state.get(WATERLOGGED).booleanValue()) {
+            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return ActionResult.success(world.isClient);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockState blockState = this.getDefaultState();
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        return (BlockState)blockState.with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return boundingShape;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
     }
 }
