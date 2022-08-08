3901139854be003984a07591f5ee94c1cf07b094
Sir Code <taylorschneider07@yahoo.com>
2022-08-08 02:46:05 -0400

    Update Fence.java

--- src/main/java/net/scmods/furniture/Fence.java
+++ src/main/java/net/scmods/furniture/Fence.java
@@ -11,11 +11,11 @@ import net.scmods.utilities.MostlyEmptyBlock;
 public class Fence extends MostlyEmptyBlock {
     protected final VoxelShape collisionShape = VoxelShapes.cuboid(0.125, 0, 0.125, 0.875, 0.5625, 0.875);
     protected final VoxelShape boundingShape = VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(VoxelShapes.union(
-                                    VoxelShapes.cuboid(0.125, 0.4375, 0.125, 0.875, 0.5625, 0.875),
-                                    VoxelShapes.cuboid(0.6875, 0, 0.1875, 0.8125, 0.4375, 0.3125)),
-                            VoxelShapes.cuboid(0.6875, 0, 0.6875, 0.8125, 0.4375, 0.8125)),
-                    VoxelShapes.cuboid(0.1875, 0, 0.6875, 0.3125, 0.4375, 0.8125)),
-            VoxelShapes.cuboid(0.1875, 0, 0.1875, 0.3125, 0.4375, 0.3125));
+        VoxelShapes.cuboid(0.125, 0.4375, 0.125, 0.875, 0.5625, 0.875),
+        VoxelShapes.cuboid(0.6875, 0, 0.1875, 0.8125, 0.4375, 0.3125)),
+        VoxelShapes.cuboid(0.6875, 0, 0.6875, 0.8125, 0.4375, 0.8125)),
+        VoxelShapes.cuboid(0.1875, 0, 0.6875, 0.3125, 0.4375, 0.8125)),
+        VoxelShapes.cuboid(0.1875, 0, 0.1875, 0.3125, 0.4375, 0.3125));
 
     public Fence(Settings settings) {
         super(settings);
