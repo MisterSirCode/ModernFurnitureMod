485a530ea0710517e72ac23a6fac99f24e72c063
Sir Code <taylorschneider07@yahoo.com>
2022-08-13 10:22:23 -0400

    Update CrateBlockEntity.java

--- src/main/java/net/scmods/furniture/CrateBlockEntity.java
+++ src/main/java/net/scmods/furniture/CrateBlockEntity.java
@@ -87,7 +87,7 @@ public class CrateBlockEntity extends LootableContainerBlockEntity {
 
     @Override
     protected Text getContainerName() {
-        return Text.translatable("container.mfm_utils.crate");
+        return Text.translatable("container.mfmutils.crate");
     }
 
     @Override
